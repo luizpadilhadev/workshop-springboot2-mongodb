eca7f4e5e3e8ecc1dcc2bb0e011aac68362ccf1a
luizpadilhadev <luizfelipepadilha86@gmail.com>
2023-02-22 20:44:04 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,6 @@ import com.nelioalves.workshopmongo.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
     
 }
--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
+import com.nelioalves.workshopmongo.resources.util.URL;
 import com.nelioalves.workshopmongo.services.PostService;
 import com.nelioalves.workshopmongo.services.UserService;
 
@@ -33,6 +35,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
 	Post obj = service.findById(id);
 	return ResponseEntity.ok().body(obj);
+    }
+    
+    @RequestMapping(value ="/titleserch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "") String text) {
+	text = URL.decodeParam(text);
+	List<Post> list = service.findByTitle(text);
+	return ResponseEntity.ok().body(list);
     }
 
 }
