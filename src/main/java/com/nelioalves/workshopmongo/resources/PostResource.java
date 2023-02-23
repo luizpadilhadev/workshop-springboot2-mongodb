9f0d17f120787741937e143fa67bdde30cb65c9d
luizpadilhadev <luizfelipepadilha86@gmail.com>
2023-02-23 18:52:57 -0300

    Consulta simples com @Query

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -37,7 +37,7 @@ public class PostResource {
 	return ResponseEntity.ok().body(obj);
     }
     
-    @RequestMapping(value ="/titleserch", method = RequestMethod.GET)
+    @RequestMapping(value ="/titlesearch", method = RequestMethod.GET)
     public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "") String text) {
 	text = URL.decodeParam(text);
 	List<Post> list = service.findByTitle(text);
